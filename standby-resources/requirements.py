8ff3b8263010bb8ee98044c7969980dfdba74149
Scrapman238 <Scrapman238@hotmail.com>
2024-10-20 21:13:46 +0100

    Check package install status

--- standby-resources/requirements.py
+++ standby-resources/requirements.py
@@ -2,6 +2,13 @@ import subprocess
 import sys
 import os
 
+def is_package_installed(package):
+    try:
+        subprocess.check_call([sys.executable, '-m', 'pip', 'show', package], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        return True
+    except subprocess.CalledProcessError:
+        return False
+
 def install_requirements(requirements_file='requirements.txt'):
     try:
         with open(requirements_file, 'r') as file:
@@ -11,10 +18,13 @@ def install_requirements(requirements_file='requirements.txt'):
         for package in packages:
             package = package.strip()
             if package:  # Skip empty lines
-                print(f"Installing {package}...")
-                subprocess.check_call([sys.executable, '-m', 'pip', 'install', package])
-        
-        print("All packages installed successfully.")
+                if not is_package_installed(package):
+                    print(f"Installing {package}...")
+                    subprocess.check_call([sys.executable, '-m', 'pip', 'install', package])
+                else:
+                    print(f"{package} is already installed.")
+
+        print("All packages processed.")
         os._exit(0)
     except Exception as e:
         print(f"An error occurred: {e}")
